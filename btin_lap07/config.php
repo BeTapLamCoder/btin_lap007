f7e911312222318ce78fd227741106cf9b068a83
DanhNguyen <nguyendanh0922@gmail.com>
2025-04-21 07:58:56 +0700

    first commit

---
+++ btin_lap07/config.php
@@ -0,0 +1,6 @@
+<?
+define("HOST", "172.21.54.11:89");
+define("DB", "db_btvn");
+define("USER", "root");
+define("PASS", "");
+
---
+++ btin_lap07/db_module.php
@@ -0,0 +1,25 @@
+<?
+require_once("config.php");
+
+function connect($link) {
+    $link - mysqli_connect(HOST, USER, PASS, DB);
+    if(mysqli_connect_errno()) {
+        echo "Loi ket noi: " . mysqli_connect_error();
+        exit();
+    }
+}
+function chayTruyVanTraVeDL($link, $sql) {
+    $result = mysqli_query($link, $sql);
+    return $result;
+}
+function chayTruyVanKhongTraVeDL($link, $sql) {
+    $result = mysqli_query($link, $sql);
+    return $result;
+}
+function xoaBoNho($link, $result) {
+    mysqli_free_result($result);
+    mysqli_close($link);
+}
+
+
+?>
