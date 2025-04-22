5c31a553f38a2dced22c09fbeab44d3411b0d048
DanhNguyen <nguyendanh0922@gmail.com>
2025-04-22 16:10:52 +0700

    fix connect

--- btin_lap07/db_module.php
+++ btin_lap07/db_module.php
@@ -2,7 +2,7 @@
 require_once("config.php");
 
 function connect($link) {
-    $link - mysqli_connect(HOST, USER, PASS, DB);
+    $link = mysqli_connect(HOST, USER, PASS, DB);
     if(mysqli_connect_errno()) {
         echo "Loi ket noi: " . mysqli_connect_error();
         exit();
@@ -16,8 +16,10 @@ function chayTruyVanKhongTraVeDL($link, $sql) {
     $result = mysqli_query($link, $sql);
     return $result;
 }
-function xoaBoNho($link, $result) {
-    mysqli_free_result($result);
+function xoaBoNho($link, $result = null) {
+    if ($result && gettype($result) === "object") {
+        mysqli_free_result($result);
+    }
     mysqli_close($link);
 }
 
